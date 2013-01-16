5cbc8a0d7ecdd33ed9ea6636d10be830bc11db05
Li Chang <leechannl@gmail.com>
2013-01-16 22:06:42 +0800

    Make `dir_traversal` run and add sublime project files.

--- c/apue/file/dir_traversal.c
+++ c/apue/file/dir_traversal.c
@@ -17,7 +17,7 @@ main(int argc, char *argv[])
 {
     int ret;
     
-    if (argc |= 2)
+    if (argc != 2)
         err_quit("usage: ftw <starting-pathname>");
 
     ret = myftw(argv[1], myfunc);   /* does it all */
@@ -25,13 +25,13 @@ main(int argc, char *argv[])
     ntot = nreg + ndir + nblk + nchr + nfifo + nslink + nsock;
     if (ntot == 0)
         ntot = 1;   /* avoid divide by 0; print 0 for all counts */
-    printf("regular files = %7ld, %5.2f %%\n", nreg, nreg*100.0/ntot);
-    printf("directories = %7ld, %5.2f %%\n", ndir, ndir*100.0/ntot);
-    printf("block special = %7ld, %5.2f %%\n", nblk, nblk*100.0/ntot);
-    printf("char special = %7ld, %5.2f %%\n", nchr, nchr*100.0/ntot);
-    printf("FIFOs = %7ld, %5.2f %%\n", nfifo, nfifo*100.0/ntot);
-    printf("symbolic links = %7ld, %5.2f %%\n", nslink, nslink*100.0/ntot);
-    printf("sockets = %7ld, %5.2f %%\n", nsock, nsock*100.0/ntot);
+    printf("regular files   = %7ld, %5.2f %%\n", nreg, nreg*100.0/ntot);
+    printf("directories     = %7ld, %5.2f %%\n", ndir, ndir*100.0/ntot);
+    printf("block special   = %7ld, %5.2f %%\n", nblk, nblk*100.0/ntot);
+    printf("char special    = %7ld, %5.2f %%\n", nchr, nchr*100.0/ntot);
+    printf("FIFOs           = %7ld, %5.2f %%\n", nfifo, nfifo*100.0/ntot);
+    printf("symbolic links  = %7ld, %5.2f %%\n", nslink, nslink*100.0/ntot);
+    printf("sockets         = %7ld, %5.2f %%\n", nsock, nsock*100.0/ntot);
 
     exit(ret);
 }
