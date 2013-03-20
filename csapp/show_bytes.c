e6aa9b4b79db202bec722f8ce1e957b1f9b12c48
leechannl <leechannl@gmail.com>
2013-03-20 09:18:38 +0800

    add csapp code example - show_bytes.

---
+++ csapp/show_bytes.c
@@ -0,0 +1,42 @@
+/* code from csapp chapter 2.1.4 figure 2.4
+
+Code to print the byte represetation of program objects. */
+
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; i ++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
+
+int main()
+{
+    int val = 12345;
+    test_show_bytes(val);
+    return 0;
+}
