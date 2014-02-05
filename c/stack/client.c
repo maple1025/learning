d47767e893e797f1a6bf5a2267d0e37cf858f607
leechannl <leechannl@gmail.com>
2014-02-05 22:12:19 +0800

    Add stack code.

---
+++ c/stack/client.c
@@ -0,0 +1,23 @@
+/*
+ * client file for stack
+ */
+
+#include "stack.h"
+
+int main(int argc, char *argv[])
+{
+    int val;
+    stack intStack;
+
+    StackNew(&intStack, sizeof(int));
+
+    for (val = 0; val < 6; val++)
+        StackPush(&intStack, &val);
+
+    while(!StackEmpty(&intStack)){
+        StackPop(&intStack, &val);
+        printf("This just popped: %d\n", val);
+    }
+    StackDispose(&intStack);
+    return 0;
+}
---
+++ c/stack/stack.c
@@ -0,0 +1,58 @@
+
+#define kInitialAllocationSize 4
+
+/*
+ * The constructor requires that the client element size to be identified up
+ * front. Since it doesn't (and will never) know the true data type.
+ */
+void StackNew(stack *s, int elemSize)
+{
+    assert(elemSize > 0);
+    s->elemSize = elemSize;
+    s->LogLength - 0;
+    s->allocLength = kInitialAllocationSize;
+    s->elems = malloc(kInitialAllocationSize * elemSize);
+    assert(s->elems != NULL);
+}
+
+/* The destructor simply needs to dispose of any resources that'll otherwise be
+ * orphande.
+ *
+ * Note that we don't call free(s). StackNew doesn't allocate space for the
+ * stack struct -- the client does! The stack destructor should noly clean up
+ * its own mess, not someone else's.
+ */
+void StackDispose(stack *s)
+{
+    free(s->elems);
+}
+
+
+bool StackEmpty(const stack *s)
+{
+    return s->LogLength == 0;
+}
+
+void StackPush(stack *s, const void *elemAddr)
+{
+    void *destAddr;
+    if (s->logLength == s->allocLength){
+        s->allocLength *= 2;
+        s->elems = realloc(s-elems, s->allocLength * s->elemSize);
+        assert(s-elems != NULL);
+    }
+
+    destAddr = (char*)s->elems + s->logLength * s->elemSize;
+    memcpy(destAddr, elemAddr, s->elemSize);
+    s->logLength++;
+}
+
+void StackPop(stack *s, void *elemAddr)
+{
+    const void *sourceAddr;
+
+    assert(!StackEmpty(s));
+    s->logLength--;
+    sourceAddr = (const char*)s->elems + s->logLength * s->elemSize;
+    memcpy(elemAddr, sourceAddr, s->elemSize);
+}
