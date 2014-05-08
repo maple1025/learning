114d6a6799f48312aed4b44512f770c736cd179b
leechannl <leechannl@gmail.com>
2014-05-08 22:23:55 +0800

    Add lisp parser.

--- js/parse_expr.js
+++ js/parse_expr.js
@@ -5,6 +5,8 @@
 
 /* jshint node: true */
 /* jshint -W084 */
+/* jshint -W069 */
+/* jshint -W054 */
 
 "use strict";
 
@@ -61,3 +63,134 @@ function parseApply(left, program) {
 
     return parseApply(expr, program.slice(1));
 }
+
+function parse(program) {
+    var result = parseExpression(program);
+    if (skipSpace(result.rest).length > 0)
+        throw new SyntaxError("Unexpected text after program");
+    return result.expr;
+}
+
+console.log(parse("+(a, 10)"));
+
+function evaluate(expr, env) {
+    switch(expr.type) {
+        case "value":
+            return expr.value;
+
+        case "word":
+            if (expr.name in env)
+                return env[expr.name];
+            else
+                throw new ReferenceError("Undefined variable: " + expr.name);
+                break;
+
+        case "apply":
+            if (expr.operator.type === "word" && expr.operator.name in specialForms)
+                return specialForms[expr.operator.name](expr.args, env);
+            var op = evaluate(expr.operator, env);
+            if (typeof op != "function")
+                throw new TypeError("Applying a non-function.");
+            return op.apply(null, expr.args.map(function(arg){
+                return evaluate(arg, env);
+            }));
+    }
+}
+
+var specialForms = Object.create(null);
+
+specialForms["if"] = function(args, env) {
+    if (args.length != 3)
+        throw new SyntaxError("Bad number of args to if");
+
+    if (evaluate(args[0], env) !== false)
+        return evaluate(args[1], env);
+    else
+        return evaluate(args[2], env);
+};
+
+specialForms["while"] = function(args, env) {
+    if (args.length != 2)
+        throw new SyntaxError("Bad number of args to while");
+
+    while(evaluate(args[0], env) !== false)
+        evaluate(args[1], env);
+
+    // Since undefined does not exist in our language, and
+    // this form does not return a meaning value, we return false
+
+    return false;
+};
+
+specialForms["do"] = function(args, env) {
+    var value = false;
+    args.forEach(function(arg){
+        value = evaluate(arg, env);
+    });
+    return value;
+};
+
+specialForms["define"] = function(args, env) {
+    if (args.length !== 2 || args[0].type !== "word")
+        throw new SyntaxError("Bad use of define");
+    var value = evaluate(args[1], env);
+    env[args[0].name] = value;
+    return value;
+};
+
+specialForms["fun"] = function(args, env) {
+    if (!args.length)
+        throw new SyntaxError("Functions need a body");
+    function name(expr) {
+        if (expr.type != "word")
+            throw new SyntaxError("Arg name must be words");
+        return expr.name;
+    }
+    var argNames = args.slice(0, args.length - 1).map(name);
+    var body = args[args.length - 1];
+
+    return function() {
+        if (arguments.length != argNames.length)
+            throw new SyntaxError("Wrong number of arguments");
+        var localEnv = Object.create(env);
+        for (var i = 0; i < arguments.length; i++)
+            localEnv[argNames[i]] = arguments[i];
+        return evaluate(body, localEnv);
+    };
+};
+
+var topEnv = Object.create(null);
+
+topEnv["true"] = true;
+topEnv["false"] = false;
+topEnv["print"] = function(value) {
+    console.log(value);
+    return value;
+};
+
+["+", "-", "*", "/", "==", "<", ">"].forEach(function(op) {
+    topEnv[op] = new Function("a, b", "return a " + op + "b;");
+});
+
+function run() {
+    var env = Object.create(topEnv);
+    var program = Array.prototype.slice.call(arguments, 0).join("\n");
+    return evaluate(parse(program), env);
+}
+
+var prog = parse("if(true, false, true)");
+console.log(prog);
+console.log(evaluate(prog, topEnv));
+
+run("do(define(total, 0),",
+    "   define(count, 1),",
+    "   while(<(count, 11),",
+    "       do(define(total, +(total, count)),",
+    "           define(count, +(count, 1)))),",
+    "   print(total))");
+
+run("do(define(pow, fun(base, exp,",
+    "   if(==(exp, 0),",
+    "       1, ",
+    "       *(base, pow(base, -(exp, 1)))))),",
+    "   print(pow(2, 10)))");
